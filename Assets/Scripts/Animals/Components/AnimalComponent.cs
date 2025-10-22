c254108181a82eb1c4f08266080239d41011345f
Arthur <artursosnin2001@gmail.com>
2025-10-22 05:26:26 +0300

    make animal pool

---
+++ Assets/Scripts/Animals/Components/AnimalComponent.cs
@@ -0,0 +1,23 @@
+﻿using StateMachine.Mono;
+using Zenject;
+
+namespace Animals.Components
+{
+    public class AnimalComponent : StateComponent, IPoolable<IMemoryPool>
+    {
+        private IMemoryPool _pool;
+
+        public void OnSpawned(IMemoryPool pool)
+        {
+            _pool = pool;
+        }
+
+        public void OnDespawned()
+        {
+            _pool = null;
+        }
+        public class Factory : PlaceholderFactory<AnimalComponent>
+        {
+        }
+    }
+}
