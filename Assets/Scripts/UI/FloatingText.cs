c78b55bc2fa3aeae02a46eae5e66d8ef0b7549b5
Arthur <artursosnin2001@gmail.com>
2025-10-28 02:55:39 +0300

    add floating text pool

---
+++ Assets/Scripts/UI/FloatingText.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using Zenject;
+
+namespace UI
+{
+    public class FloatingText : MonoBehaviour, IPoolable<string, Vector3, IMemoryPool>
+    {
+        [SerializeField]
+        private TextMeshProUGUI _text;
+        [SerializeField]
+        private float _duration = 1f;
+        [SerializeField]
+        private Vector3 _moveBy = new Vector3(0, 1f, 0);
+        [SerializeField]
+        private float _startScale = 1f;
+        [SerializeField]
+        private float _endScale = 1.15f;
+        [SerializeField]
+        private Ease _moveEase = Ease.OutCubic;
+        [SerializeField]
+        private Ease _scaleEase = Ease.OutBack;
+
+        private Sequence _sequence;
+        private IMemoryPool _pool;
+
+        private void OnDestroy()
+        {
+            _sequence?.Kill();
+        }
+
+        public void OnSpawned(string message, Vector3 position, IMemoryPool pool)
+        {
+            _pool = pool;
+
+            _text.text = message;
+            transform.localScale = Vector3.one * _startScale;
+
+            _sequence = DOTween.Sequence();
+
+            _sequence.Join(transform.DOMove(transform.position + _moveBy, _duration).SetEase(_moveEase));
+            _sequence.Join(transform.DOScale(_endScale, _duration * 0.6f).SetEase(_scaleEase));
+            _sequence.Join(_text.DOFade(0f, _duration).SetEase(Ease.Linear).SetDelay(_duration * 0.1f));
+
+            _sequence.OnComplete(() => _pool.Despawn(this));
+        }
+
+        public void OnDespawned()
+        {
+            _pool = null;
+        }
+
+        public class Factory : PlaceholderFactory<string, Vector3, FloatingText>
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Installers/FloatingTextPoolInstaller.cs
@@ -0,0 +1,27 @@
+﻿using Animals.Components;
+using UnityEditor;
+using UnityEngine;
+using Zenject;
+
+namespace UI.Installers
+{
+    public class FloatingTextPoolInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private FloatingText _prefab;
+        [SerializeField]
+        private int _poolInitialSize;
+        [SerializeField]
+        private string _transformGroupName;
+
+        public override void InstallBindings()
+        {
+            Container
+                   .BindFactory<string, Vector3, FloatingText, FloatingText.Factory>()                   
+                       .FromMonoPoolableMemoryPool(
+                            x => x.WithInitialSize(_poolInitialSize)
+                            .FromComponentInNewPrefab(_prefab)
+                            .UnderTransformGroup(_transformGroupName));
+        }
+    }
+}
